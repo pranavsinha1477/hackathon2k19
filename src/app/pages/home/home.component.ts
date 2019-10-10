c79cebd7072382837205957193bf180f1fd6ed2f
Pranav Sinha <ompranav.bindu1996@gmail.com>
2019-10-10 13:27:35 +0530

    pro

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,8 @@ import { Observable } from "rxjs/Observable";
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
